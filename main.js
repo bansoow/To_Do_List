84debff4ac05ee9143dda7f2a9b2cb031a222c3a
matteo <m.beaulieu@ecole-ipssi.net>
2021-04-14 19:58:22 +0200

    Ajout tache skin

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function addTask(){
 //Récupérer la saisie utilisateur
 let newTask = document.getElementById("add").value;
+const nouvelleEmplacementTache = document.createElement("div")
 //Récupérer le local Storage
 let localStorage = window.localStorage;
 
@@ -12,8 +13,21 @@ if(localStorage!=null){
   let compteur = parseInt(localStorage.getItem("compteur"));
   localStorage.setItem("task"+compteur, newTask);
   localStorage.setItem("compteur", compteur +1);
+  nouvelleEmplacementTache.innerHTML = `<div class="liste" id="${compteur}">
+                                          <li id="task">${newTask}
+                                          <span><button onclick="suppr()" type="button" class="clear">Suppr</button></span></li>
+                                          </div>`;
+  document.querySelector("ul").appendChild(nouvelleEmplacementTache);
     }
 //Vider l'inputPrenom
 document.getElementById("add").value = "";
 
+}
+
+function supprAll(){
+let localStorage = window.localStorage;
+if(localStorage!=null){
+localStorage.clear();
+}
+location.reload();
 }
